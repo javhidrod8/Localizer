75c328a957ed2421b4f8babcf5370739b06dcf0a
manguecor <manguecor@alum.us.es>
2022-03-28 15:25:57 +0200

    Feat: Funcionalidad editar tienda

--- src/main/java/org/springframework/samples/localizer/web/TiendaController.java
+++ src/main/java/org/springframework/samples/localizer/web/TiendaController.java
@@ -2,20 +2,26 @@ package org.springframework.samples.localizer.web;
 
 import java.util.Collection;
 import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.samples.localizer.model.Tienda;
 import org.springframework.samples.localizer.service.TiendaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TiendaController {
 	
+	private static final String VIEWS_TIENDA_CREATE_OR_UPDATE_FORM = "tiendas/createOrUpdateTiendaForm";
 	private final TiendaService tiendaService;
 	
 	@Autowired
@@ -67,6 +73,31 @@ public class TiendaController {
 		ModelAndView mav = new ModelAndView("tiendas/tiendaDetails");
 		mav.addObject(this.tiendaService.findTiendaById(tiendaId));
 		return mav;
+	}
+	
+	
+	@GetMapping(value = "/tienda/{tiendaId}/edit")
+	public String initUpdateTiendaForm(@PathVariable("tiendaId") int tiendaId, Model model) {
+		Tienda tienda = this.tiendaService.findTiendaById(tiendaId);
+		Boolean isNew=false;
+		model.addAttribute(isNew);
+		model.addAttribute(tienda);
+		return VIEWS_TIENDA_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/tienda/{tiendaId}/edit")
+	public String processUpdateTiendaForm(@Valid Tienda tienda, BindingResult result,
+			@PathVariable("tiendaId") int tiendaId,Model model) {
+		if (result.hasErrors()) {
+			Boolean isNew=false;
+			model.addAttribute(isNew);
+			return VIEWS_TIENDA_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			tienda.setId(tiendaId);
+			this.tiendaService.saveTienda(tienda);
+			return "redirect:/tienda/{tiendaId}";					
+		}
 	}
 	
 }
