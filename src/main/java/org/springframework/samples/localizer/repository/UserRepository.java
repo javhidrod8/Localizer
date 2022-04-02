4fc7d5f4e49e5916fd7e1a6a80d0a36baf5eb829
manlopalm <manlopalm@alum.us.es>
2022-04-02 12:15:27 +0200

    Feat: Creacion realizar reserva

--- src/main/java/org/springframework/samples/localizer/repository/UserRepository.java
+++ src/main/java/org/springframework/samples/localizer/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package org.springframework.samples.localizer.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.localizer.model.User;
 
 
 public interface UserRepository extends  CrudRepository<User, String>{
+	
+	@Query("SELECT user FROM User user WHERE user.username=:username")
+	public User findByUsername(@Param("username") String username);
 	
 		
 }
