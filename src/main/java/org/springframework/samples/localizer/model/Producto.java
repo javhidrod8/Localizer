3b9f67d71faabba5c3aac26ef9bc65728f263e3d
javhidrod8 <javhidrod8@gmail.com>
2022-04-05 22:23:52 +0200

    Fix: Crear y editar productos. Validaciones

--- src/main/java/org/springframework/samples/localizer/model/Producto.java
+++ src/main/java/org/springframework/samples/localizer/model/Producto.java
@@ -28,7 +28,6 @@ public class Producto extends BaseEntity {
 	@NotBlank
 	private String marca;
 
-	@NotBlank
 	private String descripcion;
 
 	@NotNull
@@ -54,7 +53,6 @@ public class Producto extends BaseEntity {
 	@JoinTable(name = "producto_intolerancias", joinColumns = @JoinColumn(name = "producto_id"), inverseJoinColumns = @JoinColumn(name = "intolerancias_id"))
 	private Set<Intolerancias> intolerancia;
 
-	@NotNull
 	private Preferencias preferencia;
 
 	@ManyToOne
@@ -62,17 +60,17 @@ public class Producto extends BaseEntity {
 	@NotNull
 	private Tienda tienda;
 
-//	public Producto(Producto producto) {
-//		this.descripcion = producto.getDescripcion();
-//		this.imagen = producto.getImagen();
-//		this.marca = producto.getMarca();
-//		this.nombre = producto.getNombre();
-//		this.precio = producto.getPrecio();
-//		this.preferencia = producto.getPreferencia();
-//		this.intolerancia = producto.getIntolerancia();
-//		this.promocionado = producto.getPromocionado();
-//		this.estado = producto.getEstado();
-//	}
+	public Producto(Producto producto) {
+		this.descripcion = producto.getDescripcion();
+		this.imagen = producto.getImagen();
+		this.marca = producto.getMarca();
+		this.nombre = producto.getNombre();
+		this.precio = producto.getPrecio();
+		this.preferencia = producto.getPreferencia();
+		this.intolerancia = producto.getIntolerancia();
+		this.promocionado = producto.getPromocionado();
+		this.estado = producto.getEstado();
+	}
 
 	public Estado getEstado() {
 		return estado;
