5ff3d44e5f02398ffc96436f436d245e0b3ffdb7
Aureliano Piqueras Rodríguez <aurpiqrod@alum.us.es>
2022-04-23 18:52:23 +0200

    Feat: Datos reales - comentario de db

--- src/main/java/org/springframework/samples/localizer/DBConfig.java
+++ src/main/java/org/springframework/samples/localizer/DBConfig.java
@@ -14,10 +14,10 @@ public class DBConfig {
     @Primary
     public DataSource mySqlDataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
         dataSourceBuilder.url("jdbc:mysql://i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/jp9vdp3kqcjnun7s?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET&createDatabaseIfNotExist=true");
         dataSourceBuilder.username("w20qhltjlvqvthm3");
         dataSourceBuilder.password("xav559iroslrjc7d");
-//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
 //        dataSourceBuilder.url("jdbc:mysql://localhost:3306/localizer?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET&createDatabaseIfNotExist=true");
 //        dataSourceBuilder.username("root");
 //        dataSourceBuilder.password("localizer");
