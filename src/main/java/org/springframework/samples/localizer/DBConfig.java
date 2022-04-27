bfc66bedf4f8cf57d9885ad8d4c0e7c6927592f9
javhidrod8 <48653257+javhidrod8@users.noreply.github.com>
2022-04-27 22:37:40 +0200

    Actualizar dbconfig para despliegue

--- src/main/java/org/springframework/samples/localizer/DBConfig.java
+++ src/main/java/org/springframework/samples/localizer/DBConfig.java
@@ -15,9 +15,9 @@ public class DBConfig {
     public DataSource mySqlDataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/aay1zk8smutg97bt?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET&createDatabaseIfNotExist=true");
-        dataSourceBuilder.username("axgoyvtmv4lwylk2");
-        dataSourceBuilder.password("yi35ivsp97x7zu3p");
+        dataSourceBuilder.url("jdbc:mysql://clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/g15ox6ff2arzl57o?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET&createDatabaseIfNotExist=true");
+        dataSourceBuilder.username("fg3jcaq19y3gfj8v");
+        dataSourceBuilder.password("x0x4flzwl4eo612g");
         return dataSourceBuilder.build();
     }
 
